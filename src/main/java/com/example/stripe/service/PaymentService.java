6bca8c83f973b4635d78a2f9f45273756b8491cd
Javed Bagwan <javed460@gmail.com>
2023-08-22 08:07:36 +0530

    Added stripe apiKey

--- src/main/java/com/example/stripe/service/PaymentService.java
+++ src/main/java/com/example/stripe/service/PaymentService.java
@@ -1,7 +1,11 @@
 package com.example.stripe.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+    @Value("${stripe.apiKey}")
+    private String stripeApiKey;
 }
