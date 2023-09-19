2c5a0d667660368a8430fb0f939027cdae12a4fb
Javed Bagwan <javed460@gmail.com>
2023-09-19 08:34:01 +0530

    Updated PaymentService

--- src/main/java/com/example/stripe/service/PaymentService.java
+++ src/main/java/com/example/stripe/service/PaymentService.java
@@ -1,5 +1,10 @@
 package com.example.stripe.service;
 
+import com.example.stripe.domain.PaymentRequest;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +14,17 @@ public class PaymentService {
     @Value("${stripe.apiKey}")
     private String stripeApiKey;
 
-    public void createPayment() {
+    public String createPayment(PaymentRequest paymentRequest) throws StripeException {
+        Stripe.apiKey = stripeApiKey;
+
+        PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
+                .setAmount(paymentRequest.getAmount())
+                .setCurrency("usd")
+                .setDescription("Example Payment")
+                .build();
+
+        PaymentIntent paymentIntent = PaymentIntent.create(createParams);
+        return paymentIntent.getId();
 
     }
 }
