02744532b8e2d4084d1e1eca22df12c3b2e17ef5
Javed Bagwan <javed460@gmail.com>
2023-09-21 09:00:55 +0530

    Renamed SpringBootStripeApplication

--- src/main/java/com/example/stripe/SpringBootStripeApplication.java
+++ src/main/java/com/example/stripe/SpringBootStripeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StripeApplication {
+public class SpringBootStripeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StripeApplication.class, args);
+		SpringApplication.run(SpringBootStripeApplication.class, args);
 	}
 
 }
